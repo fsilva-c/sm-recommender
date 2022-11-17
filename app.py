7596a84dbe67ec4242406a7f9a32875a547abc17
Filipe Silva <fsilva.cpp@gmail.com>
2022-11-16 23:08:16 -0300

    adicionado gifs para simular vídeos na aplicação

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-import tkinter as tk, time
+import os
+import tkinter as tk
+from PIL import Image
 from threading import Thread
 from random import randint
 from faker import Faker
@@ -28,35 +30,61 @@ class App(tk.Frame):
         self.icon_comment = tk.PhotoImage(file='img/icons8-comments-50.png')
         self.icon_shared  = tk.PhotoImage(file='img/icons8-share-50.png')
         self.icon_saved   = tk.PhotoImage(file='img/icons8-save-50.png')
-
+        
+        # gif...
+        random_gif = self.random_gif()
+        self.im_gif = Image.open(f'gifs/{random_gif}')
+        self.gif_frames = self.get_frames(f'gifs/{random_gif}')
+        self.gif_label = tk.Label(image='')
+        self.gif_label.grid(row=0, column=1)
+        
         # video label...
-        self.label_video = tk.Label(width=15, text=self.__video.name, font=('Arial bold', 20), pady=20)
-        self.label_video.grid(row=0, column=2)
+        self.label_video = tk.Label(width=20, text=self.__video.name, font=('Arial bold', 20), pady=20)
+        self.label_video.grid(row=1, column=1)
 
         # prev button...
         self.button_prev = tk.Button(image=self.icon_prev, command=self.__onclick_prev)
-        self.button_prev.grid(row=1, column=1)
+        self.button_prev.grid(row=2, column=0)
 
         # like button...
         self.button_like = tk.Button(image=self.icon_like, command=self.__onclick_like)
-        self.button_like.grid(row=1, column=2)
+        self.button_like.grid(row=2, column=1)
 
         # next button...
-        self.button_next = tk.Button(image=self.icon_next, command=self.__onclick_next).grid(row=1, column=3)
+        self.button_next = tk.Button(image=self.icon_next, command=self.__onclick_next).grid(row=2, column=2)
 
         # comment button...
         self.button_comment = tk.Button(image=self.icon_comment, command=self.__onclick_comment)
-        self.button_comment.grid(row=2, column=1)
+        self.button_comment.grid(row=3, column=0)
 
         # shared button...
         self.button_shared = tk.Button(image=self.icon_shared, command=self.__onclick_shared)
-        self.button_shared.grid(row=2, column=2)
+        self.button_shared.grid(row=3, column=1)
 
         # saved button...
         self.button_saved = tk.Button(image=self.icon_saved, command=self.__onclick_saved,)
-        self.button_saved.grid(row=2, column=3)
+        self.button_saved.grid(row=3, column=2)
 
         self.thread = Thread()
+        
+        self.count = 0
+        self.gif_animation()
+        
+    def random_gif(self):
+        files = os.listdir('gifs/')
+        return files[randint(0, len(files) - 1)]
+
+    def get_frames(self, gif):
+        return [tk.PhotoImage(file=gif, format=f'gif -index {i}') for i in range(self.im_gif.n_frames)]
+    
+    def gif_animation(self):
+        self.gif_label.configure(image=self.gif_frames[self.count])
+        self.count += 1
+        
+        if self.count == self.im_gif.n_frames:
+            self.count = 0
+            
+        self.after(self.im_gif.info['duration'], self.gif_animation)
 
     def recommender(self):
         self.__videos.extend([Video().get_by_id(id) for id in self.__recommender.recommender(self.user.id, self.__n_items)])
@@ -89,6 +117,12 @@ class App(tk.Frame):
 
         self.__video = self.__videos.pop(0)
         self.label_video.config(text=self.__video.name)
+        
+        # new gif...
+        self.count = 0
+        random_gif = self.random_gif()
+        self.im_gif = Image.open(f'gifs/{random_gif}')
+        self.gif_frames = self.get_frames(f'gifs/{random_gif}')
 
     # onclicks...
     def __onclick_prev(self):
--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from app import App
 
 root = Tk()
 root.title('SM Recommender')
-root.geometry('400x200')
+# root.geometry('600x600')
 app = App(root)
 root.mainloop()
